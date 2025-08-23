455a1b3a5f984db86e32032e0ce27d8c6ce10923
José Daniel Solano Gracia <daniel120802@gmail.com>
2025-08-23 09:29:19 -0500

    feat: add express dependency and update package.json for module support
    - Added "type": "module" to enable ES module syntax.
    - Introduced "express" as a new dependency.
    - Added "init" script to run the application with nodemon.

--- backEnd/App.js
+++ backEnd/App.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+main();
 async function main() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const app = express();
     const PORT = 3000;
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    
+    app.get('/api/productos', (req, res) => {
+        res.json(["Manzana", "Banana", "Naranja"]);
     });
-    app.listen(PORT, () => {
-        console.log(`Servidor escuchando en http://localhost:${PORT}`);
+
+    app.get('/test', (req, res) => {
+        res.json("Hola Jeremias el mas pro del html");
     });
+    
+    process.stdout.write('\x1Bc');
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 }
